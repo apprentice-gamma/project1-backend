40805b069741ccc2b71906f78bd7b8897722ecac
Greg Santikian <gsantikian@gmail.com>
2015-04-02 17:35:06 -0400

    short end of the stick

--- app/models/user.js
+++ app/models/user.js
@@ -1,15 +1,15 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
-// var Bookmark
+var Bookmark     = mongoose.Bookmark
 
 var UserSchema   = new Schema({
     firstname: String,
     lastname: String,
     email: String,
-    bookmarks: [new Schema(url: String,
-                           description: String,
-                           title: String)]
-    // bookmarks: [Bookmark.schema]
+    // bookmarks: [new Schema(url: String,
+    //                        description: String,
+    //                        title: String)]
+    bookmarks: [Bookmark.schema]
 });
 
 module.exports = mongoose.model('User', UserSchema);
