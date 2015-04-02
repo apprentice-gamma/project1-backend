174caa48376d8e611dc925e634ca71ebcdc2fa52
Greg Santikian <gsantikian@gmail.com>
2015-04-02 16:21:58 -0400

    Removes bears adds bookmarks

---
+++ app/models/bookmarks.js
@@ -0,0 +1,10 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var BookmarkSchema = new Schema({
+  url: String,
+  description: String,
+  title: String
+});
+
+module.exports = mongoose.model('Bookmark', BookmarkSchema)
--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,12 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
+var Bookmark     = require("Bookmark")
 
 var UserSchema   = new Schema({
     firstname: String,
     lastname: String,
-    email: String
+    email: String,
+    bookmarks: [Bookmark.schema]
 });
 
 module.exports = mongoose.model('User', UserSchema);
