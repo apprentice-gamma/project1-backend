b9ae278e1f18453df18ca7a0272db10d3929f43f
Greg Santikian <gsantikian@gmail.com>
2015-04-02 16:39:44 -0400

    Adds a comment

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,7 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 // var Bookmark     = require("Bookmark")
-
+// a comment
 var UserSchema   = new Schema({
     firstname: String,
     lastname: String,
