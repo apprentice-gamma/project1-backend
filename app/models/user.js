78e4dfb51b68ed993d5f85c055ea08323a5a6a29
Greg Santikian <gsantikian@gmail.com>
2015-04-02 16:24:42 -0400

    comments out bookmarks code

--- app/models/user.js
+++ app/models/user.js
@@ -1,12 +1,12 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
-var Bookmark     = require("Bookmark")
+// var Bookmark     = require("Bookmark")
 
 var UserSchema   = new Schema({
     firstname: String,
     lastname: String,
-    email: String,
-    bookmarks: [Bookmark.schema]
+    email: String
+    // bookmarks: [Bookmark.schema]
 });
 
 module.exports = mongoose.model('User', UserSchema);
