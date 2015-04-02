d0dfa968a13fe6d337625971719b4fce0746c16a
Greg Santikian <gsantikian@gmail.com>
2015-04-02 17:43:07 -0400

    maybe it is the ./

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,6 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
-var Bookmark     = require('bookmark.js')
+var Bookmark     = require('./bookmark.js')
 
 var UserSchema   = new Schema({
     firstname: String,
