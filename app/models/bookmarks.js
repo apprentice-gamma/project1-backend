8bd42effb69dfd31163e02d347ff7d020029a7e3
Greg Santikian <gsantikian@gmail.com>
2015-04-02 16:52:06 -0400

    adds bookmark schema a second time

--- app/models/bookmarks.js
+++ app/models/bookmarks.js
@@ -1,10 +1,10 @@
-// var mongoose     = require('mongoose');
-// var Schema       = mongoose.Schema;
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
 
-// var BookmarkSchema = new Schema({
-//   url: String,
-//   description: String,
-//   title: String
-// });
+var BookmarkSchema = new Schema({
+  url: String,
+  description: String,
+  title: String
+});
 
-// module.exports = mongoose.model('Bookmark', BookmarkSchema)
+module.exports = mongoose.model('Bookmark', BookmarkSchema);
