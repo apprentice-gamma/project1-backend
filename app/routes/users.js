ebc45282c608a35ad74bae2c6a294f139adffa3e
Greg Santikian <gsantikian@gmail.com>
2015-04-08 10:17:19 -0400

    Fixes formatting adds proper semicolons

--- app/routes/users.js
+++ app/routes/users.js
@@ -19,7 +19,6 @@ router.route('/users')
         message: 'User created!'
       });
     });
-
   })
 
   // get all the users (accessed at GET http://localhost:8080/api/users)
@@ -40,55 +39,53 @@ router.route('/users/:user_id')
     User.findById(req.params.user_id, function(err, user) {
       if (err)
         res.send(err);
+
       res.json(user);
     });
   })
-  
+
   .delete(function(req, res){
-        User.remove({
-            _id: req.params.user_id
-        }, function(err, user){
-            if(err)
-                res.send(err)
-            res.json({message: 'Successfully deleted user.'})
-        });
+    User.remove({_id: req.params.user_id}, function(err, user){
+      if(err)
+        res.send(err);
+
+      res.json({message: 'Successfully deleted user.'});
     });
+  });
 
 router.route('/users/:user_id/shoutouts')
   // get the user with that id (accessed at GET http://localhost:8080/api/users/:user_id)
   .post(function(req, res) {
-      User.findById(req.params.user_id, function(err, user) {
+    User.findById(req.params.user_id, function(err, user) {
+      var shoutout = new Shoutout();
+      shoutout.text = req.body.text;
+      user.shoutouts.push(shoutout);
 
-        var shoutout = new Shoutout();
-        shoutout.text = req.body.text;
-        user.shoutouts.push(shoutout);
+      user.save(function(err) {
+        if (err)
+          res.send(err);
 
-        user.save(function(err) {
-          if (err)
-            res.send(err);
-
-          res.json({
-            message: 'Shoutout created!'
-          });
+        res.json({
+          message: 'Shoutout created!'
         });
-
       });
-    })
+    });
+  });
 
 router.route('/users/:user_id/shoutouts/:shoutout_id')
 
   .delete(function(req, res){
-    
     User.findById(req.params.user_id, function(err, user) {
       user.shoutouts.pull(req.params.shoutout_id);
       user.save(function(err) {
-          if (err)
-            res.send(err);
+        if (err)
+          res.send(err);
 
-          res.json({
-            message: 'Successfully deleted shoutout.'
-          });
+        res.json({
+          message: 'Successfully deleted shoutout.'
         });
+      });
     });
- });
+  });
+
 module.exports = router;
