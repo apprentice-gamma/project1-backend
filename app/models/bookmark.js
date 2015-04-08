a114ecf162486b57996302834ac2f2f437bf0193
Greg Santikian <gsantikian@gmail.com>
2015-04-08 13:17:30 -0400

    Adds validations to bookmarks and includes mongoose-unique-validator

--- app/models/bookmark.js
+++ app/models/bookmark.js
@@ -1,14 +1,17 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-var Comment      = require('./comment.js');
+var mongoose        = require('mongoose');
+var Schema          = mongoose.Schema;
+var Comment         = require('./comment.js');
+var uniqueValidator = require('mongoose-unique-validator');
 
 var BookmarkSchema = new Schema({
-  url: String,
-  description: String,
-  title: String,
-  user: String,
-  comments: [Comment.schema],
-  date: { type : Date, default: Date.now }
+  url:         { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  title:       { type: String, required: true },
+  user:        { type: String, required: true },
+  comments:    [ Comment.schema ],
+  date:        { type : Date, default: Date.now }
 });
+
+BookmarkSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Bookmark', BookmarkSchema);
